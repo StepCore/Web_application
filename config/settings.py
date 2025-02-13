aa4f3fae400d5806c1afd06a9982174007ad4180
Stepan <galantov251016@gmail.com>
2025-02-13 18:51:10 +0500

    create new app blog_app; update settings.py, urls.py

--- config/settings.py
+++ config/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "online_store",
+    "blog_app",
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("online_store.urls", namespace="online_store")),
+    path("blogs/", include("blog_app.urls", namespace="blog_app")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
