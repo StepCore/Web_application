5df124b5a23642e5b25aa4623bb286508cb11207
Stepan <galantov251016@gmail.com>
2025-02-26 02:14:56 +0500

    fix settings.py

--- config/settings.py
+++ config/settings.py
@@ -107,10 +107,10 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
 
-EMAIL_HOST = "smtp.yandex.ru"
-EMAIL_PORT = 465
-EMAIL_HOST_USER = "stpwn@yandex.ru"
-EMAIL_HOST_PASSWORD = "tycegxxoljlnybmu"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
