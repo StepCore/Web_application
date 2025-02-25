106028902e88e36d65d3c3a0cd9e20dc2e62ae16
Stepan <galantov251016@gmail.com>
2025-02-26 01:37:21 +0500

    create users app, urls, views...

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "online_store",
     "blog_app",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -98,3 +99,20 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+AUTH_USER_MODEL = "users.User"
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
+
+EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "stpwn@yandex.ru"
+EMAIL_HOST_PASSWORD = "tycegxxoljlnybmu"
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
